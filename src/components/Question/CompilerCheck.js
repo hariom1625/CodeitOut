0d7f8ed7e9eeebc60b3358461c7a03665b22c3f3
Hari Om Yadav <hariomyadav.btech.cs19@iiitranchi.ac.in>
2021-03-06 22:21:55 +0530

    add verdict

--- src/components/Question/CompilerCheck.js
+++ src/components/Question/CompilerCheck.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './CompilerCheck.css';
+import './Compiler.css';
 import MonacoEditor from 'react-monaco-editor';
 import axios from 'axios';
 
@@ -13,27 +13,16 @@ class Compiler extends React.Component {
                   input: localStorage.getItem('input') || ``,
                   output: ``,
                   userInput: ``,
-                  language_id: localStorage.getItem('language_id') || 54,
-answers:[],
-tc:0,
-ans:0
+                  language_id: localStorage.getItem('language_id') || 54
             };
       }
 
       componentDidMount() {
-            axios.get("https://codeitoutserver.herokuapp.com/answer.json").then((res) => {
+            axios.get("http://localhost:4000/").then((res) => {
 
-                  this.setState({answers: res.data});
-
-console.log(this.state.answers);
-
-
+                  this.setState({users: res.data});
             })
-
-
       }
-
-
       editorDidMount(editor, monaco) {
             console.log('editorDidMount', editor);
             editor.focus();
@@ -53,10 +42,7 @@ console.log(this.state.answers);
 
       userInput = (event) => {
             event.preventDefault();
-
             this.setState({user_input: event.target.value});
-            console.log(this.state.user_input);
-
       };
 
       language = (event) => {
@@ -67,183 +53,83 @@ console.log(this.state.answers);
 
       };
 
-            run = async (e) => {
-                  e.preventDefault();
-                  let outputText = document.getElementById("output");
-                  outputText.innerHTML = "";
-                  outputText.innerHTML += "Creating Submission\n";
-                  const response = await fetch("https://judge0-ce.p.rapidapi.com/submissions", {
-                        method: "POST",
-                        headers: {
-                              "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
-                              "x-rapidapi-key": "e535d12d26msh1fe73d90a985d7ap1b0b2ajsnacb9e34ab20e",
-                              "content-type": "application/json",
-                              accept: "application/json"
-                        },
-                        body: JSON.stringify({source_code: this.state.input, stdin: this.state.user_input, language_id: this.state.language_id})
-                  });
+      submit = async (e) => {
+            e.preventDefault();
+            let outputText = document.getElementById("output");
+            outputText.innerHTML = "";
+            outputText.innerHTML += "Creating Submission\n";
+            const response = await fetch("https://judge0-ce.p.rapidapi.com/submissions", {
+                  method: "POST",
+                  headers: {
+                        "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
+                        "x-rapidapi-key": "e535d12d26msh1fe73d90a985d7ap1b0b2ajsnacb9e34ab20e",
+                        "content-type": "application/json",
+                        accept: "application/json"
+                  },
+                  body: JSON.stringify({source_code: this.state.input, stdin: this.state.user_input, language_id: this.state.language_id})
+            });
 
-                  outputText.innerHTML += "Submission Created/n";
-                  const jsonResponse = await response.json();
+            outputText.innerHTML += "Submission Created/n";
+            const jsonResponse = await response.json();
 
-                  let jsonGetSolution = {
+            let jsonGetSolution = {
 
-                        status: {
-                              description: "Queue"
-                        },
-                        stderr: null,
-                        compile_output: null
-                  };
-
-                  while (jsonGetSolution.status.description !== "Accepted" && jsonGetSolution.stderr == null && jsonGetSolution.compile_output == null) {
-
-                        outputText.innerHTML = `Status: ${jsonGetSolution.status.description}`;
-
-                        if (jsonResponse.token) {
-                              let url = `https://judge0-ce.p.rapidapi.com/submissions/${jsonResponse.token}?base64_encoded=true`;
-
-                              const getSolution = await fetch(url, {
-                                    method: "GET",
-                                    headers: {
-                                          "x-rapidapi-key": "e535d12d26msh1fe73d90a985d7ap1b0b2ajsnacb9e34ab20e",
-                                          "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
-                                          "content-type": "application/json"
-                                    }
-                              });
-
-                              jsonGetSolution = await getSolution.json();
-
-                        }
-
-                  }
-
-                  if (jsonGetSolution.stdout) {
-                        const output = atob(jsonGetSolution.stdout);
-
-                        const test = this.state.users.message;
-
-                        outputText.innerHTML = "";
-
-                        outputText.innerHTML += `Output:\n${output}\nExecution Time: ${jsonGetSolution.time} Secs\nMemory Used : ${jsonGetSolution.memory} bytes`;
-                        if (output === test)
-                              outputText.innerHTML += "\n\nCorrect Answer";
-                        else
-                              outputText.innerHTML += "\n\nWrong Answer";
-
-                        }
-                  else if (jsonGetSolution.stderr) {
-                        const error = atob(jsonGetSolution.stderr);
-
-                        outputText.innerHTML = "";
-                        outputText.innerHTML += `\n Error : ${error}`;
-                  } else {
-                        const compilation_error = atob(jsonGetSolution.compile_output);
-
-                        outputText.innerHTML = "";
-                        outputText.innerHTML += `\n Error: ${compilation_error}`;
-                  }
-
+                  status: {
+                        description: "Queue"
+                  },
+                  stderr: null,
+                  compile_output: null
             };
 
-            submit = async (e) => {
-                  e.preventDefault();
+            while (jsonGetSolution.status.description !== "Accepted" && jsonGetSolution.stderr == null && jsonGetSolution.compile_output == null) {
 
-                  let outputText = document.getElementById("output");
+                  outputText.innerHTML = `Status: ${jsonGetSolution.status.description}`;
+
+                  if (jsonResponse.token) {
+                        let url = `https://judge0-ce.p.rapidapi.com/submissions/${jsonResponse.token}?base64_encoded=true`;
+
+                        const getSolution = await fetch(url, {
+                              method: "GET",
+                              headers: {
+                                    "x-rapidapi-key": "e535d12d26msh1fe73d90a985d7ap1b0b2ajsnacb9e34ab20e",
+                                    "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
+                                    "content-type": "application/json"
+                              }
+                        });
+
+                        jsonGetSolution = await getSolution.json();
+
+                  }
+
+            }
+
+            if (jsonGetSolution.stdout) {
+                  const output = atob(jsonGetSolution.stdout);
+
+                  const test = this.state.users.message;
+
                   outputText.innerHTML = "";
-                  outputText.innerHTML += "Creating Submission\n";
 
-                  const response = await fetch("https://judge0-ce.p.rapidapi.com/submissions", {
-                        method: "POST",
-                        headers: {
-                              "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
-                              "x-rapidapi-key": "e535d12d26msh1fe73d90a985d7ap1b0b2ajsnacb9e34ab20e",
-                              "content-type": "application/json",
-                              accept: "application/json"
-                        },
-
-                        body: JSON.stringify({source_code: this.state.input, stdin: this.props.tc, language_id: this.state.language_id})
-                  });
-
-                  outputText.innerHTML += "Submission Created/n";
-                  const jsonResponse = await response.json();
-
-                  let jsonGetSolution = {
-
-                        status: {
-                              description: "Queue"
-                        },
-                        stderr: null,
-                        compile_output: null
-                  };
-
-                  while (jsonGetSolution.status.description !== "Accepted" && jsonGetSolution.stderr == null && jsonGetSolution.compile_output == null) {
-
-                        outputText.innerHTML = `Status: ${jsonGetSolution.status.description}`;
-
-                        if (jsonResponse.token) {
-                              let url = `https://judge0-ce.p.rapidapi.com/submissions/${jsonResponse.token}?base64_encoded=true`;
-
-                              const getSolution = await fetch(url, {
-                                    method: "GET",
-                                    headers: {
-                                          "x-rapidapi-key": "e535d12d26msh1fe73d90a985d7ap1b0b2ajsnacb9e34ab20e",
-                                          "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
-                                          "content-type": "application/json"
-                                    }
-                              });
-
-                              jsonGetSolution = await getSolution.json();
-
-                        }
+                  outputText.innerHTML += `Output:\n${output}\nExecution Time: ${jsonGetSolution.time} Secs\nMemory Used : ${jsonGetSolution.memory} bytes`;
+                  if (output === test)
+                        outputText.innerHTML += "\n\nCorrect Answer";
+                  else
+                        outputText.innerHTML += "\n\nWrong Answer";
 
                   }
+            else if (jsonGetSolution.stderr) {
+                  const error = atob(jsonGetSolution.stderr);
 
-                  if (jsonGetSolution.stdout) {
-                        const output = atob(jsonGetSolution.stdout);
+                  outputText.innerHTML = "";
+                  outputText.innerHTML += `\n Error : ${error}`;
+            } else {
+                  const compilation_error = atob(jsonGetSolution.compile_output);
 
-      const test = JSON.stringify(output);
-      const t1 = JSON.stringify(this.props.ans)
+                  outputText.innerHTML = "";
+                  outputText.innerHTML += `\n Error: ${compilation_error}`;
+            }
 
-      //
-      //
-      // {
-      // this.state.answers.map((answer)=>{
-      //
-      // if(this.props.ansid===answer.id){
-      // return(
-      // this.setState({
-      // ans:answer.ans,
-      // tc:answer.tc})
-      // )
-      // }
-      // })
-      // }
-      // const tc1 = JSON.stringify(this.state.tc)
-      //
-      // console.log(t1);
-      // console.log(tc1);
-                        outputText.innerHTML = "";
-
-                        outputText.innerHTML += `Output:\n${output}\nExecution Time: ${jsonGetSolution.time} Secs\nMemory Used : ${jsonGetSolution.memory} bytes`;
-                        if (t1===test)
-                              outputText.innerHTML += "\n\nCorrect Answer";
-                        else
-                              outputText.innerHTML += "\n\nWrong Answer";
-                        }
-                  else if (jsonGetSolution.stderr) {
-                        const error = atob(jsonGetSolution.stderr);
-
-                        outputText.innerHTML = "";
-                        outputText.innerHTML += `\n Error : ${error}`;
-                  } else {
-                        const compilation_error = atob(jsonGetSolution.compile_output);
-
-                        outputText.innerHTML = "";
-                        outputText.innerHTML += `\n Error: ${compilation_error}`;
-                  }
-
-            };
-
+      };
       render() {
 
             const options = {
@@ -270,13 +156,8 @@ console.log(this.state.answers);
                                     </span>
                               </label>
                               <MonacoEditor className="monaco" width="650" height="500" language={this.state.language} theme="vs-dark" options={options} value={this.state.input} onChange={this.onChange} editorDidMount={this.editorDidMount}/>
-
-
-                                    <button type="submit" className="btn btn-dark run-btn btn-lg" onClick={this.run}>
-                                                                        <i class="fas run-icon fa-cogs"></i>Run
-                                                                  </button>
-<button type="submit" className="btn btn-dark run-btn btn-lg" onClick={ this.submit}>
-                                    <i class="fas run-icon fa-cogs"></i>Submit
+                              <button type="submit" className="btn btn-dark run-btn btn-lg" onClick={this.submit}>
+                                    <i class="fas run-icon fa-cogs"></i>Run
                               </button>
                               <label for="tags">
                                     <b className="language-opt">Language</b>
